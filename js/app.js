3f5978149a0e57effcc5c44f5fb7f3571cb30e96
Gabbytam <gtam2@fordham.edu>
2020-10-07 20:09:50 -0700

    found correct path to access image and able to get url to change depending on user input

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
 console.log('confusion');
 
-let requestURL= 'http://www.reddit.com/search.json?q=cats+nsfw:no';
+let requestURL= 'http://www.reddit.com/search.json?q=';
+let makeItAppropriate= '+nsfw:no';
+
 const fetchImg= (searchTerm)=> {
-    fetch(requestURL /*+searchTerm*/)
+    fetch(requestURL+ inputBox.value+ makeItAppropriate)
     .then((responseData)=> {
         return responseData.json(); //will returned the responseData in json format
     })
     .then((jsonData)=> { //can then do a then on the returned responseData in json format, pass in as parameter jsonData
         console.log('heres the jsonData:', jsonData);
-        console.log('follow the trail:', jsonData.data.children[0].data.preview.images[0].source.url);
+        console.log('inputbox value', inputBox.value);
+        console.log('follow the trail:', jsonData.data.children[0].data.thumbnail);
     })
     .catch((error)=> {
         console.log('Here is your error sir/maam:', error);
@@ -23,3 +26,6 @@ document.addEventListener('DOMContentLoaded', ()=> {
         fetchImg();
     })
 })
+
+//children is an array, may have to iterate through that to access several images for cats
+//maybe make that into its own function that can be called, definitely going to need to set variables for things 
