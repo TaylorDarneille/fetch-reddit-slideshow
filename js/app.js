4644b1025ab42e2f05383be836a430e04f4c8f2d
lind1125 <danieljlinden@gmail.com>
2020-10-07 21:53:00 -0500

    wrote function to fetch and display thumbnail

--- js/app.js
+++ js/app.js
@@ -1,10 +1,32 @@
 console.log('SCRIPT IS CONNECTED')
 const requestUrl = 'https://www.reddit.com/search.json?q='
 
+const displayImage = (result) => {
+        let container = document.querySelector('#container')
+        container.innerHTML = `
+            <img src="${result.children[0].data.thumbnail}" class="new-image">
+        `    
+}
+
+fetchStuff = (inputValue) => {
+    fetch(requestUrl+inputValue)
+    .then((responseData)=>{
+        return responseData.json()
+    })
+    .then((jsonData)=>{
+        let result = jsonData.data
+        console.log(result)
+        displayImage(result)
+    })
+    .catch((error)=>{
+        console.log('Oops! There is an error:', error)
+    })
+}
+
 document.addEventListener('DOMContentLoaded', ()=>{
     
     form.addEventListener('submit', (e)=>{
         e.preventDefault()
-        fetchStuff(requestUrl+input.value)
+        fetchStuff(input.value)
     })
 })
