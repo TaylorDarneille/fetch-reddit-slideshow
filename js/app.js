917707aec4ccddd9ed5dabd58a1fe32fa6c27d90
Gabbytam <gtam2@fordham.edu>
2020-10-07 19:48:59 -0700

    got basic fetch call and put it in a function

--- js/app.js
+++ js/app.js
@@ -1,9 +1,25 @@
 console.log('confusion');
 
+let requestURL= 'http://www.reddit.com/search.json?q=cats+nsfw:no';
+const fetchImg= (searchTerm)=> {
+    fetch(requestURL /*+searchTerm*/)
+    .then((responseData)=> {
+        return responseData.json(); //will returned the responseData in json format
+    })
+    .then((jsonData)=> { //can then do a then on the returned responseData in json format, pass in as parameter jsonData
+        console.log('heres the jsonData:', jsonData);
+        console.log('follow the trail:', jsonData.data.children[0].data.preview.images[0].source.url);
+    })
+    .catch((error)=> {
+        console.log('Here is your error sir/maam:', error);
+    })
+}
+
 document.addEventListener('DOMContentLoaded', ()=> {
     form.addEventListener('submit', (event)=> {
         event.preventDefault();
         container.style.display= 'none';
         console.log('submitted');
+        fetchImg();
     })
 })
