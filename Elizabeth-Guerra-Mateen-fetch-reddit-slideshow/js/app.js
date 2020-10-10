0071428cfd93aa204b35c1fe916914313e2eaa43
Elizabeth Guerra <ecguerra@outlook.com>
2020-10-09 23:30:13 -0400

    slideshow is working!

--- Elizabeth-Guerra-Mateen-fetch-reddit-slideshow/js/app.js
+++ Elizabeth-Guerra-Mateen-fetch-reddit-slideshow/js/app.js
@@ -1,5 +1,8 @@
 
-const requestUrl = 'https://www.reddit.com/search.json?q=kittens'
+const requestUrl = 'https://www.reddit.com/search.json?q=kittens+nsfw:no'
+const slideshow = document.querySelector('#slideshow')
+let i = 0
+const imgArray = []
 
 createSlideShow = () => {
 fetch(requestUrl)
@@ -7,32 +10,65 @@ fetch(requestUrl)
     return responseData.json()
 })
 .then((jsonData) => {
-    // console.log(jsonData.data.children)
+// console.log(jsonData.data.children)
     const dataChildren = jsonData.data.children
-//    console.log(dataChildren)
-    let slideIndex = 0
+//  console.log(dataChildren)
     getImages(dataChildren)
-    // let slideshow = document.querySelector('#slideshow')
-    // slideshow.innerHTML=`<button>stop</button>`
+    showSlides()
+    // const stop = document.createElement('button')
+    // stop.innerText = 'stop slideshow'
+    // slideshow.appendChild(stop)
+    document.querySelector('#stop-container').innerHTML=`<button>stop slideshow</button>`
 })
 .catch((err) => {
     console.log('ERROR',err)
 })
 
 }
+
+// set up the query images and put them in an array
+// I'm not sure this is necessary, but so far it's helpful
     const getImages = (dataChildren) => {
-        let slideshow = document.querySelector('#slideshow')
         for(let i = 0; i < dataChildren.length; i++) {
-            console.log(dataChildren[i])
-            slideshow.innerHTML = `<div>
-            <img src= ${dataChildren[i].data.url}>
-            </div>`
-            // dataChildren[i].data.url.style.display = 'none'
+            // console.log(dataChildren[i])
+            // so many broken images - this only gives me 3 though // but if I use innerHTML it does go through all
+            if(dataChildren[i].data.url.endsWith('.jpg') || dataChildren[i].data.url.endsWith('.png')) {
+                // let img = document.createElement('img')
+                // img.setAttribute('id',`img${i}`)
+                // img.setAttribute('src',dataChildren[i].data.url)
+                let img = dataChildren[i].data.url
+                // img.classList.add('display-none')
+                imgArray.push(img)
+            }
+            // slideshow.appendChild(img)
+            
+            //dataChildren[i].data.url.style.display = 'none'
         }
-    //     slideIndex++
-    //     if (slideIndex > dataChildren.length) {slideIndex = 1}
-    //     dataChildren[slideIndex-1].style.display = 'block'
-    //     setTimeout(getImages, 2000)
+        return(imgArray)
+    }
+
+// Need to briefly display each image in the array, but then clear it before the next one comes though
+    const showSlides = () => {
+        // this goes straight to the last image, but the rest are all still in the elements
+        // so the setTimeout needs to be somewhere in here
+        // for (let i=0; i<imgArray.length; i++) {
+            // This is still going through everything too fast
+            // setTimeout(()=>{
+            //    if(i>0) document.querySelector (`#img${i - 1}`).classList.add('display-none')
+            //     slideshow.appendChild(imgArray[i])
+            //     document.querySelector(`#img${i}`).classList.remove('display-none')
+
+            // while (i < imgArray.length) {
+                slideshow.innerHTML = 
+                `<div>
+                    <img src='${imgArray[i]}'>
+                </div>`
+                i++
+                if (i>imgArray.length) {i=0}
+            // }
+            setTimeout(showSlides,2000)
+        //    }, 2000)
+        // }
     }
 
 document.addEventListener('DOMContentLoaded',()=>{
