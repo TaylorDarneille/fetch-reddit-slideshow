c3fc6beab48d06c014a33783f87a25e330c5f3b0
regularvanessaperson <vamavila4@gmail.com>
2020-10-10 19:24:33 -0500

    thumbnails show up on the page

--- js/app.js
+++ js/app.js
@@ -1,33 +1,40 @@
-const requestURL = "http://www.reddit.com/search.json?q=cats+nsfw:no"
+const requestURL = "http://www.reddit.com/search.json?q=nsfw:no+"
+const conatainer = document.querySelector(".container")
 
 
-   
 
 document.addEventListener("DOMContentLoaded", ()=>{
+    form.addEventListener("submit", (event)=>{
+        event.preventDefault()
+        console.log("user input is:", input.value)
+        fetchPicture()
+    })
+
+
+const fetchPicture =()=> {
     fetch(requestURL+input.value)
     .then((responseData)=>{
         return responseData.json()
     })
     .then((jsonData)=>{
-        let pic = document.createElement("img")
         const children =jsonData.data.children
-        children.forEach((child)=>{
-            getPhoto(child)
-        })
+        console.log(jsonData)
+        children.forEach(getPhoto)
         console.log(jsonData)
     })
     .catch((err)=>{
         console.log("error:", err)
     })
-    const fetching = () =>{
-        fetch(requestURL+input.value)
-    }
+}
 })
 
 // const buttonClick = () =>{
-
+    //picture slideshow starts
 // }
 const getPhoto = (child)=>{
-  console.log(child.data.thumbnail)
+    let pic = document.createElement("img")
+        pic.setAttribute("src", child.data.thumbnail)
+        conatainer.appendChild(pic)
+        console.log(pic)
+    return child.data.thumbnail
 }
-
