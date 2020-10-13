168c2245189f2c6d8c7301eb83a406f178c51f4f
Gabbytam <gtam2@fordham.edu>
2020-10-13 12:52:11 -0700

    added fading style for images

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-console.log('confusion');
 
 let requestURL= 'http://www.reddit.com/search.json?q=';
 let makeItAppropriate= '+nsfw:no';
@@ -11,7 +10,6 @@ const getImgURL= (info)=> { //info represents the jsonData that gets passed in w
     info.data.children.forEach(child=> { //info.data.children is an array so we can call .forEach() on it. child represents each array element 
         imgArr.push(child.data.thumbnail); //reach the imgURL by child.data.thumbnail and adding it to the imgArr
     })
-    console.log('heres the imgArr',imgArr);
 }
 
 //FUNCTION THAT CREATES AN IMG ELEMENT AND APPENDS IT, GETS CALLED BY SETINTERVAL
@@ -38,6 +36,7 @@ const displayImg= ()=> {
         imgBox.removeChild(imgBox.firstChild); //gets rid of img
         showHideBox.style.display= ''; //displays the input option
         putBtn.removeChild(putBtn.firstChild); //gets rid of the stop button
+        // showHideBox.classList.add('fadeBox');
     })
     
 }
@@ -45,13 +44,14 @@ const displayImg= ()=> {
 //FUNCTION THAT WILL FETCH JSON DATA, IS CALLED WITH SUBMIT EVENT LISTENER
 const fetchImg= ()=> {
     imgArr= []; //reset imgArr to be empty for every new search
+    imgBox.innerText= 'Images loading...';
     fetch(requestURL+ inputBox.value+ makeItAppropriate)
     .then((responseData)=> {
         return responseData.json(); //will returned the responseData in json format
     })
     .then((jsonData)=> { //can then do a then on the returned responseData in json format, pass in as parameter jsonData
         getImgURL(jsonData); //call getImgURL passing in the jsonData so the function has access to it   
-        switchPic= setInterval(displayImg, 2000); //set a setInterval that calls the function in charge of creating img elements, setting them, and adding them
+        switchPic= setInterval(displayImg, 3000); //set a setInterval that calls the function in charge of creating img elements, setting them, and adding them
     })
     .catch((error)=> {
         console.log('Here is your error sir/maam:', error);
@@ -65,9 +65,4 @@ document.addEventListener('DOMContentLoaded', ()=> {
         fetchImg(); //calling the function that contains our fetch request 
     })
 })
-
-
-
-// To do: 
-// Display animation / slideshow of images (with DOM manipulation)
 
