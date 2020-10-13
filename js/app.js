dbd4262689e89d07007c261329262e5a4a926d1d
lind1125 <danieljlinden@gmail.com>
2020-10-13 01:00:33 -0500

    built stop button functionality

--- js/app.js
+++ js/app.js
@@ -3,21 +3,24 @@ const requestUrl = 'https://www.reddit.com/search.json?q='
 const container = document.querySelector('#container')
 const h1 = document.querySelector('h1')
 const h3 = document.querySelector('h3')
-// const stopBtn = document.stop
+let imgArr
+let interval
 
+const slideShow = () => {
+    let i = Math.floor(Math.random() * imgArr.length)
+    console.log(i)
+    container.innerHTML = `<img src="${imgArr[i]}" class="displayed-image">`}
 
 const displayImage = (result) => {
     form.style.display = 'none'
     h1.style.display = 'none'
     h3.style.display = 'none'
     stopBtn.style.display = 'block'
-    let usable = result.filter(item => item.data.thumbnail !== 'self')
-    const imgArr = usable.map(item => item.data.thumbnail)  
+    let firstFilter = result.filter(item => item.data.thumbnail !== 'self')
+    let usable = firstFilter.filter(item => item.data.thumbnail !== 'default')
+    imgArr = usable.map(item => item.data.thumbnail)  
     console.log(imgArr)
-    setInterval(function(){
-        let i = Math.floor(Math.random() * imgArr.length)
-        container.innerHTML = `<img src="${imgArr[i]}" class="displayed-image">`}
-        , 3000)
+    interval = setInterval(slideShow, 2000)
 }
 
 fetchStuff = (inputValue) => {
@@ -27,7 +30,7 @@ fetchStuff = (inputValue) => {
     })
     .then((jsonData)=>{
         let result = jsonData.data.children
-        console.log(result)
+        console.log(result[3].data.thumbnail)
         displayImage(result)
     })
     .catch((error)=>{
@@ -36,7 +39,13 @@ fetchStuff = (inputValue) => {
 }
 
 resetPage = () => {
-
+    h1.style.removeProperty('display')
+    h3.style.removeProperty('display')
+    form.style.removeProperty('display')
+    stopBtn.style.display = 'none'
+    clearInterval(interval)
+    container.removeChild(container.firstChild)
+    form.reset()
 }
 
 document.addEventListener('DOMContentLoaded', ()=>{
@@ -45,5 +54,5 @@ document.addEventListener('DOMContentLoaded', ()=>{
         e.preventDefault()
         fetchStuff(input.value)
     })
-    document.addEventListener('click', resetPage)
+    stopBtn.addEventListener('click', resetPage)
 })
