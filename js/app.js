733f1c5e9c9fd08dd86762c556590dc82d780ed2
xlnl <lamanhnle@gmail.com>
2020-10-13 16:03:22 -0400

    lightyears progress so far, trying to figure out how to workout slideshow

--- js/app.js
+++ js/app.js
@@ -1,103 +1,85 @@
+const requestUrl = "https://www.reddit.com/search.json?nsfw:no&q="
+let currentImg = []
+let picIndex = 0;
+let timer = null;
+const delayTimer = 3000;
 
+let form = document.getElementById("form");
 
-
-// use innerHTML with ${""}
-
-
-// const fetchPush = () => {
-
-//     fetch(requestUrl+input.value)
-//     .then((responseData)=>{
-//         return responseData.json()
-//     })
-//     .then((jsonData)=>{
-//         console.log("jsonData.results:", jsonData.results)
-//     })
-//     .catch((err)=>{
-//         console.log("on no!", err)
-//     })
-// }
-
-// document.addEventListener('DOMContentLoaded', ()=>{
-//     form.addEventListener('button', (e)=>{
-//         e.preventDefault()
-//         console.log("fetch me my boy", input.value)
-//         fetchPush()
-//     })
-// })
-
-
-// change results number, just change the url!
-
-// document.addEventListener("DOMContentLoaded", ()=>{
-
-//     form.addEventListener('submit', (e)=>{
-//         e.preventDefault()
-//         fetchPics(requestUrl+input.value)
-//         console.log("fetch me my boy", input.value)
-//         // fetchPics()
-//     })
-
-//     const fetchPics = (endpoint) => { 
-//         fetch(endpoint)
-//         .then((responseData)=>{
-//             return responseData.json();        
-//         })
-//         .then((jsonData)=>{
-//             console.log(jsonData);
-//             let pics = jsonData.results;
-//             while(results.firstChild) {
-//                 results.removeChild(results.firstChild)
-//             }
-//             pics.forEach(addPics);
-//         })
-//         .catch((error)=>{
-//             console.log("error!!!:", error);        
-//         });
-//     }
-    
-//     const addPics = (pic)=>{
-//         let results = document.getElementById("results");
-//         console.log(results)
-//         let img = document.createElement("img");
-//         let link = `${data.children.data.preview.images.source.url}`;
-//         console.log(link);
-//         img.setAttribute("src", link);
-//         results.appendChild(img);
-//     }
-// });
-
-
-document.addEventListener("DOMContentLoaded", ()=>{
-
-    const requestUrl = "https://www.reddit.com/search.json?q=pusheen"
-
-    const addPics = (pic)=>{
-        let link = `${data.children.data.preview.images.source.url}`;
-        console.log(link);
-        let img = document.createElement("img");
-        img.setAttribute("src", link)
-        let results = document.getElementById("results");
-        results.appendChild(img)
+form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    let input = document.getElementById("input").value;
+    console.log("fetch me my pic:", input)
+    if (input) {
+        fetchPics(input);
     }
+    else {
+        console.log("try again!")
+    }    
+})
 
-    fetch(requestUrl)
+const fetchPics = (input) => { 
+    console.log("reddit search for:", input)
+    fetch(requestUrl + input)
     .then((responseData)=>{
-        // Fetch will package the response into an object with some methods that allow us to do some useful things with the response.
-        // Use the .json method to return the data in JSON format
-        return responseData.json();
+        return responseData.json();        
     })
     .then((jsonData)=>{
-        // the above .then passed our returned data into this callback
         console.log(jsonData);
-        // now we can see that the data we want is nested under an inner 'results'
-        console.log(jsonData.results)
-        // store this array of objects in a 'people' variable
-        let people = jsonData.results;
-        people.forEach(addPerson);
+        let picsResults = jsonData.data.children.filter((pic) => {
+            return pic.data.post_hint = "image";
+        }) .map((pic) => {
+            return {
+                url: pic.data.url
+            }
+        })
+        console.log(picsResults);
+        currentImg = picsResults;
     })
     .catch((error)=>{
-        // If any error is sent bac, you will have access to it here.
-        console.log("error!!!:", error);
+        console.log("error!!!:", error);        
     });
-});
+}
+
+// rendering the pics 
+const showpic = () => {
+    results.innerHTML = "";
+    let img = document.createElement("img");
+    img.setAttribute("src", currentImg[picIndex].url);
+    results.append(img);
+}
+
+// interval - timer to change pic
+const changepic = () => {
+    picIndex += 1
+    if (picIndex >= currentImg.length) {
+        picIndex = 0;
+    }
+    showPic(); // repeat
+}
+
+// start slideshow
+const startSlide = () => {
+    console.log("does this work???");
+    let form = document.getElementById("form-container");
+    let slideshow = document.getElementById("slideshow-container");
+    form.style.display = "none";
+    slideshow.style.display = "inline-block"
+    showPic();
+    timer = setInterval(changePic, delayTimer)
+}
+
+
+
+// stop slideshow
+
+let stop = document.getElementById("stop")
+stop.addEventListener("click", () => {
+    clearInterval(timer);
+    let form = document.getElementById("form-container");
+    let slideshow = document.getElementById("slideshow-container");
+    form.style.display = "none";
+    slideshow.style.display = "inline-block"
+    picIndex = 0;
+})
+
